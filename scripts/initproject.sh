cf8adb60f83f90610df87e268b23806ef29386c2
wilfred-kun <hilgertjuuhbosgewoon@hotmail.com>
2021-03-12 10:20:01 +0100

    Add stub for initproject script

---
+++ scripts/initproject.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# `selector` needs to catch SIGTERM
+fwdsigterm() {
+    kill -TERM "$1" 2> /dev/null
+}
+
+LANGUAGES=(
+    Python
+    C
+    CPP
+)
+
+trap fwdsigterm $! 2
+language=$(selector "Enter a language:" ${LANGUAGES[*]})
+EXIT=$?
+trap 2
+
+if [[ $EXIT != "0" ]] ; then
+    echo "Exiting..."
+    exit 1
+fi
