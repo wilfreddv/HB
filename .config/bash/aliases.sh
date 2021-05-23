859072c8a0d0619223166341e6aa3764903b5b13
wilfreddv <hilgertjuuhbosgewoon@hotmail.com>
2021-05-23 15:11:32 +0200

    Add function to clone specific directory from git repo

--- .config/bash/aliases.sh
+++ .config/bash/aliases.sh
@@ -1,1 +1,16 @@
 alias gs="git status -s"
+
+function git_directory {
+    USAGE="git_directory REPO SUBDIRECTORY"
+    repo=$1
+    subdir=$2
+    dest=`basename $repo .git`
+    if [ -z "$repo" ] || [ -z "$subdir" ]; then
+        echo $USAGE
+        return 1;
+    fi
+
+    git clone --depth 1 $repo $dest
+    cd $dest
+    git filter-branch --prune-empty --subdirectory-filter $subdir HEAD
+}
